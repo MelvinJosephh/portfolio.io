81e46b24588aeb9555560150e05796458ee7bc5e
MelvinJosephh <collinsme.2000@gmail.com>
2024-11-23 19:05:19 +0300

    made some minor changes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,14 @@ require("dotenv").config();
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+// Configure CORS
+const corsOptions = {
+    origin: ["https://aberrange-server.netlify.app", "http://localhost:3000"],
+    methods: "GET,POST,PUT,DELETE", // Specify allowed HTTP methods
+    allowedHeaders: "Content-Type,Authorization", // Specify allowed headers
+  };
+
+app.use(cors(corsOptions));
 
 // Connect to the database
 connectDB();
